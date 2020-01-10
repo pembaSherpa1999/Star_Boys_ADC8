4dbac71ecbe90291f10684a0a4255336a0450a60
Raju144 <58839887+Raju144@users.noreply.github.com>
2020-01-10 22:03:15 +0545

    adding update and delete file

--- ADC8_ADipIT02_Hospital_Management_System/Hospital/views.py
+++ ADC8_ADipIT02_Hospital_Management_System/Hospital/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+# View function file for Update functionality
+
+def update_record(request,id):
+    instance = Hospital.objects.get(id=id)
+
+    # Update the form with the data obtained above
+
+    form = HospitalForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request, 'update.html', {'form': form})
+
+
+
+# ...index.html file(Use this code in index.html page)
+
+# {#This is the loop through the Hospital db and display individual records#}
+    {% for hospital in hospitals %}
+        <li> {{ hospital.patient_first_name }} {{ hospital.patient_middle_name }} {{ hospital.patient_last_name }}  {{ hospital.patient_age }}</li>
+
+# {#this add the line below#}
+<a href="{% url 'delete' Hospital.id %}">Delete Record</a>
+
+# ...views.py file(View function for Delete)
+def delete_record(request,id):
+    # this get the object from the Hospital db
+    record = Hospital.objects.get(id=id)
+    record.delete()
+    return redirect('index')
+    
