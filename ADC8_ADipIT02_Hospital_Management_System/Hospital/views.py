6b410f33718ac3da6e8a94214841ffe08477d1ea
Diplak4 <59081797+Diplak4@users.noreply.github.com>
2020-01-10 23:50:00 +0545

    adding read file

--- ADC8_ADipIT02_Hospital_Management_System/Hospital/views.py
+++ ADC8_ADipIT02_Hospital_Management_System/Hospital/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+        #views.py file
+def index(request):
+#gets all patients
+    patients = patient.objects.all()
+    context = {
+        'patients': patients
+    }
+# pass the context object to be rendered in the index file
+    return render(request, 'index.html', context)
+
+          #index.html file
+{#loop through the patient db and display individual records#}
+    {% for patient in patients %}
+        <li> {{ patientt.first_name }} {{ patientt.last_name }} {{ patient.major }}  patient.year }}</li>
+    {% endfor %}
