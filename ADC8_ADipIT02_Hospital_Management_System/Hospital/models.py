e04ca182275a859aa2bdc9988b18701afd9a3837
Raju144 <58839887+Raju144@users.noreply.github.com>
2020-02-10 14:19:16 +0545

    Adding Permission File

--- ADC8_ADipIT02_Hospital_Management_System/Hospital/models.py
+++ ADC8_ADipIT02_Hospital_Management_System/Hospital/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
-# Create your models here.
+# model used for Permission
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class patient(models.Model):
+    patientName = models.CharField(max_length=250)
+    patientAddress = models.CharField(max_length=20)
+    patientPhoneNo = models.IntegerField()
+    patientAge = models.IntegerField()
+    patientSex = models.CharField(max_length=20)
+    
+    
+    def __str__(self):  
+        return  self.patientName +""+ self.patientAddress +""+str(self.patientPhoneNo)+""+ str(self.patientAge)
+    
+    def is_valid_patient(self):
+
+        return (self.patientName !=self.patientSex) and (self.patientAge >=0)
